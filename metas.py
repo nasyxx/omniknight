2a23abc2acecd14da4b6bae30de6a777ada8fc2e
Nasy <nasyxx+git@gmail.com>
2019-06-07 17:31:08 +0800

    Update PIN message link.

--- metas.py
+++ metas.py
@@ -57,7 +57,7 @@ PASTES = [
     "https://hastebin.com/",
 ]
 
-PIN = "https://t.me/pythonzh/231108"
+PIN = "https://t.me/pythonzh/231313"
 
 DOCS = [
     "[官方文档](https://docs.python.org/zh-cn/3/)",
